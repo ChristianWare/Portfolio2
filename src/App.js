4c0c3e84780103ed91e26327939791b268628bbf
Chris <chris.ware.dev@gmail.com>
2021-07-05 14:57:30 -0700

    5. Adding Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import AboutUS from "./pages/AboutUS";
+import GlobalStyle from "./components/GlobalStyle";
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyle />
       <AboutUS />
     </div>
   );
