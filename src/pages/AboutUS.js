40141fb206dab198f82ea52aff0bd8e06f628512
Chris <chris.ware.dev@gmail.com>
2021-07-05 14:09:38 -0700

    3. Services Section

--- src/pages/AboutUS.js
+++ src/pages/AboutUS.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
 
 const AboutUS = () => {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+    </>
   );
 };
 
